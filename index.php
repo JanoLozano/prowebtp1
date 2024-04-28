40ae718207bfc65c88a4cdf5769e9e2a7c432299
JanoLozano <jano.lozano@davinci.edu.ar>
2024-04-28 14:35:56 -0300

    pruba de estilos
    agregue css pero no se si asi va a ser la version final

--- index.php
+++ index.php
@@ -1,20 +1,43 @@
 <?php
 require_once("aldeano.php");
 require_once("arbusto.php");
-
-// Instanciacion Aldeano - Arbusto
-$aldeano = new Aldeano();
-$arbusto = new Arbusto();
-
-// Ejecucion
-$aldeano->recolectar($arbusto);
-
 require_once("pesquero.php");
 require_once("bancodepesca.php");
-// Instanciacion  Pesquero--BancoDePesca
-$pesquero = new Pesquero();
-$bancoDePesca = new BancoDePesca();
-
-// Ejecucion
-$pesquero->recolectar($bancoDePesca);
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP1</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <div class="container">
+        <h1>El Aldeano Y El Pesquero</h1>
+        
+        <div class="recolectar">
+            <?php
+            // Instanciacion Aldeano - Arbusto
+            $aldeano = new Aldeano();
+            $arbusto = new Arbusto();
+
+            // Ejecucion
+            $aldeano->recolectar($arbusto);
+            ?>
+        </div>
+
+        <div class="recolectar">
+            <?php
+            // Instanciacion  Pesquero--BancoDePesca
+            $pesquero = new Pesquero();
+            $bancoDePesca = new BancoDePesca();
+
+            // Ejecucion
+            $pesquero->recolectar($bancoDePesca);
+            ?>
+        </div>
+    </div>
+</body>
+</html>
